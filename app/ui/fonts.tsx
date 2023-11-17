cd4233de200089ed934584dcd15029b7a48338cc
AriNavarro23 <navarro.a.ariel@hotmail.com>
2023-11-17 17:09:16 -0300

    first

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,8 @@
+import {  Anek_Odia, Lusitana  } from 'next/font/google';
+
+export const anek = Anek_Odia({ subsets: ['latin']});
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
